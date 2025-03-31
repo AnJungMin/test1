f733822e525365d0f34947f457054c6eb2d67511
AnJungMin <dkswjdals4027@gmail.com>
2025-03-31 13:58:47 +0900

    model.py fix2

--- app/model/model.py
+++ app/model/model.py
@@ -3,33 +3,63 @@ import torch.nn as nn
 from torchvision import models
 
 class MultiTaskMobileNetV3(nn.Module):
-    def __init__(self, pretrained=True):
+    def __init__(self):
         super(MultiTaskMobileNetV3, self).__init__()
-        # 사전학습된 MobileNetV3-Large 백본 불러오기
-        self.backbone = models.mobilenet_v3_large(pretrained=pretrained)
+        # pretrained MobileNetV3-Large 백본 불러오기
+        self.backbone = models.mobilenet_v3_large(pretrained=True)
         num_ftrs = self.backbone.classifier[0].in_features
-        
+
         # 마지막 classifier 제거 -> feature vector 추출
         self.backbone.classifier = nn.Identity()
-        
-        # 각 태스크별 classifier 정의 (6개의 질환 분류)
-        self.fc_layers = nn.ModuleList([
-            self._create_fc_layer(num_ftrs) for _ in range(6)
-        ])
 
-    def _create_fc_layer(self, num_ftrs):
-        return nn.Sequential(
+        # 각 태스크별 classifier 정의
+        self.backbone.fc1 = nn.Sequential(
             nn.Linear(num_ftrs, num_ftrs),
-            nn.Hardswish(),  # 혹은 ReLU 등
+            nn.Hardswish(),
             nn.Dropout(p=0.2),
-            nn.Linear(num_ftrs, 4)  # 클래스 수: 0~3
+            nn.Linear(num_ftrs, 4)
+        )
+        self.backbone.fc2 = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2),
+            nn.Linear(num_ftrs, 4)
+        )
+        self.backbone.fc3 = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2),
+            nn.Linear(num_ftrs, 4)
+        )
+        self.backbone.fc4 = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2),
+            nn.Linear(num_ftrs, 4)
+        )
+        self.backbone.fc5 = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2),
+            nn.Linear(num_ftrs, 4)
+        )
+        self.backbone.fc6 = nn.Sequential(
+            nn.Linear(num_ftrs, num_ftrs),
+            nn.Hardswish(),
+            nn.Dropout(p=0.2),
+            nn.Linear(num_ftrs, 4)
         )
 
     def forward(self, x):
         # feature 추출
         features = self.backbone(x)
-        
-        # 각 태스크별 결과
-        task_outputs = [fc_layer(features) for fc_layer in self.fc_layers]
-        
-        return tuple(task_outputs)
+
+        # 태스크별 분류
+        mise_head = self.backbone.fc1(features)
+        pizi_head = self.backbone.fc2(features)
+        mosa_head = self.backbone.fc3(features)
+        mono_head = self.backbone.fc4(features)
+        biddem_head = self.backbone.fc5(features)
+        talmo_head = self.backbone.fc6(features)
+
+        return mise_head, pizi_head, mosa_head, mono_head, biddem_head, talmo_head
